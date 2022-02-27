ba5826ce86659000ece9afaea2c11b27232b8dd8
Vikram Singh <vikram.m.singh@case.edu>
2022-02-26 19:21:55 -0500

    Modifying a few more files

--- leads/models.py
+++ leads/models.py
@@ -51,28 +51,28 @@ class LeadManager(models.Manager):
         return super().get_queryset()
 
 
-class Lead(models.Model):
-	first_name = models.CharField(max_length=20)
-	last_name = models.CharField(max_length=20)
-	age = models.IntegerField(default=0)
-	user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-	agent = models.ForeignKey(
+#class Lead(models.Model):
+#	first_name = models.CharField(max_length=20)
+#	last_name = models.CharField(max_length=20)
+#	age = models.IntegerField(default=0)
+#	user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+#	agent = models.ForeignKey(
 	    "Agent", null=True, blank=True, on_delete=models.SET_NULL)
-	category = models.ForeignKey(
+#	category = models.ForeignKey(
 	    "Category", related_name="leads", null=True, blank=True, on_delete=models.SET_NULL)
-	description = models.TextField()
-	date_added = models.DateTimeField(auto_now_add=True)
-	phone_number = models.CharField(max_length=13)
-	email = models.EmailField()
-	profile_picture = models.ImageField(
-	    null=True, blank=True, upload_to="profile_pictures/")
-	converted_date = models.DateTimeField(null=True, blank=True)
-	source = models.CharField(max_length=100, null=True, blank=True)
+#	description = models.TextField()
+#	date_added = models.DateTimeField(auto_now_add=True)
+#	phone_number = models.CharField(max_length=13)
+#	email = models.EmailField()
+#	profile_picture = models.ImageField(
+#	    null=True, blank=True, upload_to="profile_pictures/")
+#	converted_date = models.DateTimeField(null=True, blank=True)
+#	source = models.CharField(max_length=100, null=True, blank=True)
 
-	objects = LeadManager()
+#	objects = LeadManager()
 
-	def __str__(self):
-		return f"{self.first_name} {self.last_name}"
+#	def __str__(self):
+#		return f"{self.first_name} {self.last_name}"
 
 #class Lead_Invoice(models.Model):
 #	user = models.ForeignKey(User, related_name='user_lead_invoice', on_delete=models.CASCADE)
@@ -105,35 +105,35 @@ class Lead(models.Model):
 #		return str(self.user)
 
 
-def handle_upload_follow_ups(instance, filename):
-    return f"lead_followups/lead_{instance.lead.pk}/{filename}"
+#def handle_upload_follow_ups(instance, filename):
+#    return f"lead_followups/lead_{instance.lead.pk}/{filename}"
 
 
-class FollowUp(models.Model):
-	user = models.ForeignKey(User, related_name="user_followup", on_delete=models.CASCADE)
-	lead = models.ForeignKey(Lead, related_name="followups", on_delete=models.CASCADE)
-	date_added = models.DateTimeField(auto_now_add=True)
-	notes = models.TextField(blank=True, null=True)
-	file = models.FileField(null=True, blank=True,upload_to=handle_upload_follow_ups)
+#class FollowUp(models.Model):
+#	user = models.ForeignKey(User, related_name="user_followup", on_delete=models.CASCADE)
+#	lead = models.ForeignKey(Lead, related_name="followups", on_delete=models.CASCADE)
+#	date_added = models.DateTimeField(auto_now_add=True)
+#	notes = models.TextField(blank=True, null=True)
+#	file = models.FileField(null=True, blank=True,upload_to=handle_upload_follow_ups)
 	
-	def __str__(self):
-		return f"{self.lead.first_name} {self.lead.last_name}"
+#	def __str__(self):
+#		return f"{self.lead.first_name} {self.lead.last_name}"
 
 
-class Agent(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+#class Agent(models.Model):
+#	user = models.OneToOneField(User, on_delete=models.CASCADE)
+#	user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.user.email
+#	def __str__(self):
+#		return self.user.email
 
 
-class Category(models.Model):
-	name = models.CharField(max_length=30)
-	user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+#class Category(models.Model):
+#	name = models.CharField(max_length=30)
+#	user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.name
+#	def __str__(self):
+#		return self.name
 
 #class InvoiceLeads(models.Model):
 #	user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -156,7 +156,7 @@ class Category(models.Model):
 def post_user_signal(sender,instance,created, **kwargs):
 	if created:
 		UserProfile.objects.create(user=instance )
-		Agent.objects.create(user=instance, user_profile=instance.userprofile)
+#		Agent.objects.create(user=instance, user_profile=instance.userprofile)
 
 post_save.connect(post_user_signal, sender=User)
 
